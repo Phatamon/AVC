612ae8d0acce40861931fe331195360178dc8604
Phatamon <47975484+Phatamon@users.noreply.github.com>
2020-07-11 20:13:43 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,10 +3,11 @@ const discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
 
     let embed = new discord.RichEmbed()
+        .setAuthor(`${bot.user.username} Command`, bot.user.avatarURL)
         .setColor('RANDOM')
         .setThumbnail(bot.user.avatarURL)
-        .setTitle(`♨️Help Command♨️`)
-        .setDescription('**Bot Auto Crete Vice Channel official Server ◣Have Fun Country◥** :relaxed:   ``-=- Help -=- <>: MUST, []: OPTIONAL``')
+        .setTitle('Prefix : `Hfc.`')
+        .setDescription('**Bot Auto Create Voice Channel official Server ◣Have Fun Country◥** :relaxed:   ``-=- Help -=- <>: MUST, []: OPTIONAL``')
         .addField(`● lock`, 'Untuk mengunci voice channel kamu')
         .addField(`● unlock`, 'Untuk membuka voice channel kamu')
         .addField(`● name`, 'Untuk mengubah nama voice channel kamu')
