18b902251c8949af407c2251dd7f31a93b00db11
Phatamon <47975484+Phatamon@users.noreply.github.com>
2020-07-12 17:48:05 +0700

    Update unlock.js

--- cmds/unlock.js
+++ cmds/unlock.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args,) => {
     //let ow = message.channel.permissionOverwrites.get(message.author.id)
     if (message.channel.name !== "《setting-rooms》")
     return message.reply("Gunakan Di 《setting-rooms》");
-    if(message.channel.permissionsFor(message.author).has('MANAGE_CHANNELS')=== true) {
+    if(VC.permissionsFor(message.author).has('MANAGE_CHANNELS')) {
         VC.overwritePermissions(message.guild.roles.find('name', '@everyone'), {
             'CONNECT': true
         }); 
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args,) => {
        
     } else{
     // if(message.channel.permissionsFor(message.author).has('MANAGE_CHANNELS')=== true) 
-     return message.channel.send("Only owner channel can use this command") 
+     return message.reply("Only owner channel can use this command") 
 
     }
     
