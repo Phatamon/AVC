47d7b8ae066133c60969c2b0a555599b0429f294
Phatamon <47975484+Phatamon@users.noreply.github.com>
2020-07-12 17:42:20 +0700

    Update kick.js

--- cmds/kick.js
+++ cmds/kick.js
@@ -7,9 +7,13 @@ module.exports.run = async (bot, message, args,) => {
        // V//C.overwritePermissions(message.guild.roles.find('name', '@everyone'), {
             //'CONNECT': false
        // });   
-
+if(VC.permissionsFor(message.author).has('MANAGE_CHANNELS')) {
        rMember.setVoiceChannel(null)
         return message.reply(`Successfully kick ${rMember} from your channel`)
-    
+    } else{
+    // if(message.channel.permissionsFor(message.author).has('MANAGE_CHANNELS')=== true) 
+     return message.reply("Only owner channel can use this command") 
+
+    }
     
 }
