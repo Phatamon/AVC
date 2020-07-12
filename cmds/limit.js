e0dc4c45a1c3ab6c4317d431bbf4382a1513994f
Phatamon <47975484+Phatamon@users.noreply.github.com>
2020-07-12 17:41:00 +0700

    Update limit.js

--- cmds/limit.js
+++ cmds/limit.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args,) => {
     //let ow = message.channel.permissionOverwrites.get(message.author.id)
     if (message.channel.name !== "《setting-rooms》")
     return message.reply("Gunakan Di 《setting-rooms》");
-    if(message.channel.permissionsFor(message.author).has('MANAGE_CHANNELS')=== true) {
+    if(VC.permissionsFor(message.author).has('MANAGE_CHANNELS')) {
         VC.setUserLimit(args[0]);
         message.reply(`Successfully set the channel to limit ${args[0]}`)
     
