ba7a65178eed3cf7c9abc8c0fd397838db253880
Phatamon <47975484+Phatamon@users.noreply.github.com>
2020-07-12 17:47:32 +0700

    Update unlimit.js

--- cmds/unlimit.js
+++ cmds/unlimit.js
@@ -3,14 +3,14 @@ module.exports.run = async (bot, message, args,) => {
     //let ow = message.channel.permissionOverwrites.get(message.author.id)
     if (message.channel.name !== "《setting-rooms》")
     return message.reply("Gunakan Di 《setting-rooms》");
-    if(message.channel.permissionsFor(message.author).has('MANAGE_CHANNELS')=== true) {
+    if(VC.permissionsFor(message.author).has('MANAGE_CHANNELS')) {
         VC.setUserLimit(null);
         message.reply(`Successfully set the channel to not limit`)
     
        
-    } else{
+   } else{
     // if(message.channel.permissionsFor(message.author).has('MANAGE_CHANNELS')=== true) 
-     return message.channel.send("Only owner channel can use this command") 
+     return message.reply("Only owner channel can use this command") 
 
     }
     
